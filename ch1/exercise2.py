95d1ce0019fb4ec4ed8444b76da76409b438bc75
glennrzhang <glennrzhang@gmail.com>
2020-07-18 19:23:56 +0800

    e2com

---
+++ ch1/exercise2.py
@@ -0,0 +1,7 @@
+#THIS IS A STUFF ON HOW TO USE COMMENTS IN PYTHON
+'''
+THIS IS HOW TO MAKE MULTI LINE COMMENTS
+'''
+print("glenn is practicing how to make comments in coding")
+#if I did the comments from earlier correct than this shall work and I will only see the thing in the print in the terminal
+print("Yay, it worked")
