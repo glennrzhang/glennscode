f1bab9fe74eed0390e69088993919eaf0bd44068
glennrzhang <glennrzhang@gmail.com>
2020-07-19 17:15:29 +0800

    c

--- ch1/exercise3.py
+++ ch1/exercise3.py
@@ -6,3 +6,9 @@ cars_not_driven=cars-drivers
 cars_driven=drivers
 carpool_capacity=cars_driven*space_in_a_car
 average_passengers_per_car=passengers/cars_driven
+
+
+print(f"There are {cars} cars available.")
+print(f"There are only {drivers} drivers available.")
+print(f"Ther will be {cars_not_driven} empty cars today")
+print(f"We can transport {carpool_capacity} people today.")
