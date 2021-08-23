69dfa0885687f1057339aab7170bedb586e95b5e
Glenn Zhang <glennrzhang@gmail.com>
2021-08-23 19:20:54 +0800

    lol

--- challengen.py
+++ challengen.py
@@ -45,4 +45,15 @@ def draw_a_christmas_tree(leaf_size,trunk_x,trunk_y):
         print(string_n_2_0)
         num_5 += 1
 
+def parallelogram_printer(parallelogram_x,parallelogram_y):
+    space_counter = parallelogram_y-1
+    reapeted_times = 0
+    while reapeted_times < parallelogram_y:
+        print(" "*space_counter + "*"*parallelogram_x)
+        reapeted_times += 1
+        space_counter += -1
+
+
 diamond_printer(10)
+
+parallelogram_printer(10,20)
